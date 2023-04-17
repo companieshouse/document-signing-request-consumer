c196bf123e1f1fcaa43873057f4cad8c42f26966
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2023-04-17 17:14:19 +0100

    DCAC-75: Bring in generated test code too.

---
+++ src/test/java/uk/gov/companieshouse/documentsigningrequestconsumer/ConsumerAspect.java
@@ -0,0 +1,29 @@
+package uk.gov.companieshouse.documentsigningrequestconsumer;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CountDownLatch;
+
+@Aspect
+@Component
+public class ConsumerAspect {
+
+    private CountDownLatch latch;
+
+    public ConsumerAspect(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    @After("execution(* uk.gov.companieshouse.documentsigningrequestconsumer.Consumer.consume(..))")
+    void afterConsume(JoinPoint joinPoint) {
+        latch.countDown();
+    }
+
+    @After("execution(* uk.gov.companieshouse.documentsigningrequestconsumer.ErrorConsumer.consume(..))")
+    void afterErrorConsume(JoinPoint joinPoint) {
+        latch.countDown();
+    }
+}

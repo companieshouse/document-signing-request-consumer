25cee6d677289cfae62e3438546682281672fd1b
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2023-04-19 17:28:50 +0100

    DCAC-75: Fix test broken by archetype generated code.

--- src/test/java/uk/gov/companieshouse/documentsigningrequestconsumer/DocumentSigningRequestConsumerApplicationTests.java
+++ src/test/java/uk/gov/companieshouse/documentsigningrequestconsumer/DocumentSigningRequestConsumerApplicationTests.java
@@ -2,8 +2,16 @@ package uk.gov.companieshouse.documentsigningrequestconsumer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test_main_positive.yml")
+@Import(TestConfig.class)
+@EmbeddedKafka
+@ActiveProfiles("test_main_positive")
 class DocumentSigningRequestConsumerApplicationTests {
 
     @Test

5cfedaa32f7d5d310a49caad95243b920259216c
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2023-04-27 17:12:33 +0100

    DCAC-75: Give test message plausible field values.

--- src/test/java/uk/gov/companieshouse/documentsigningrequestconsumer/Constants.java
+++ src/test/java/uk/gov/companieshouse/documentsigningrequestconsumer/Constants.java
@@ -8,10 +8,10 @@ public class Constants {
     }
 
     public static final SignDigitalDocument DOCUMENT = SignDigitalDocument.newBuilder()
-            .setOrderNumber("1")
-            .setPrivateS3Location("TODO")
-            .setDocumentType("TODO")
-            .setItemGroup("TODO")
+            .setOrderNumber("ORD-152416-079544")
+            .setPrivateS3Location("s3://document-api-images-cidev/docs/--EdB7fbldt5oujK6Nz7jZ3hGj_x6vW8Q_2gQTyjWBM/application-pdf")
+            .setDocumentType("363s")
+            .setItemGroup("ORD-152416-079544-1")
             .build();
 
     public static final String SAME_PARTITION_KEY = "key";

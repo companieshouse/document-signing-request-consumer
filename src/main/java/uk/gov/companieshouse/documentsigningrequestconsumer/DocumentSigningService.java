260c6a0e33d4a348f9f8db2fe2dc6474b1064bf4
caismith-ch <csmith2@companieshouse.gov.uk>
2023-05-05 13:14:58 +0100

    Basic outline of how to send request to document signing api

--- src/main/java/uk/gov/companieshouse/documentsigningrequestconsumer/DocumentSigningService.java
+++ src/main/java/uk/gov/companieshouse/documentsigningrequestconsumer/DocumentSigningService.java
@@ -1,6 +1,11 @@
 package uk.gov.companieshouse.documentsigningrequestconsumer;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriTemplate;
+import uk.gov.companieshouse.api.error.ApiErrorResponseException;
+import uk.gov.companieshouse.api.handler.exception.URIValidationException;
+import uk.gov.companieshouse.api.model.documentsigning.SignPDFApi;
+import uk.gov.companieshouse.api.model.documentsigning.SignPDFResponseApi;
 
 /**
  * Makes sign PDF requests to the Document Signing API.
@@ -8,8 +13,30 @@ import org.springframework.stereotype.Component;
 @Component
 class DocumentSigningService implements Service {
 
+    private ApiClientService apiClientService;
+
+    private static final String SIGN_PDF_URI = "/document-signing/sign-pdf";
+
+    public DocumentSigningService(ApiClientService apiClientService) {
+        this.apiClientService = apiClientService;
+    }
+
     @Override
     public void processMessage(ServiceParameters parameters) {
-        // TODO Implement this
+        SignPDFApi requestBody = new SignPDFApi();
+
+        // I assume we will access the data like the following
+        //requestBody.setDocumentLocation(parameters.getData().get("id").toString());
+
+        SignPDFResponseApi signPDFResponseApi;
+
+        try {
+            signPDFResponseApi = apiClientService.getInternalApiClient()
+                            .privateDocumentSigningResourceHandler()
+                            .signPDF(SIGN_PDF_URI, requestBody)
+                            .execute().getData();
+        } catch (ApiErrorResponseException | URIValidationException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

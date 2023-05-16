f9c2d7c4a15ed5f5bd8db9558811f4732e95f146
caismith-ch <csmith2@companieshouse.gov.uk>
2023-05-16 18:01:10 +0100

    Update Constants to include latest fields added to kafka topic

--- src/test/java/uk/gov/companieshouse/documentsigningrequestconsumer/Constants.java
+++ src/test/java/uk/gov/companieshouse/documentsigningrequestconsumer/Constants.java
@@ -12,6 +12,10 @@ public class Constants {
             .setPrivateS3Location("s3://document-api-images-cidev/docs/--EdB7fbldt5oujK6Nz7jZ3hGj_x6vW8Q_2gQTyjWBM/application-pdf")
             .setDocumentType("363s")
             .setItemGroup("ORD-152416-079544-1")
+            .setCompanyName("Test Company")
+            .setCompanyNumber("00000000")
+            .setFilingHistoryDescription("A test filing history document")
+            .setFilingHistoryType("AM01")
             .build();
 
     public static final String SAME_PARTITION_KEY = "key";

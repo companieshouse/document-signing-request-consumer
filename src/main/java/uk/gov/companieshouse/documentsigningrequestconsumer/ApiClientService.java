31ec5d6d372b4597be9d7c7c00f6c1c74822c265
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2023-07-12 10:21:19 +0100

    DCAC-151: Make internal API URL required for sign PDF call configurable.

--- src/main/java/uk/gov/companieshouse/documentsigningrequestconsumer/ApiClientService.java
+++ src/main/java/uk/gov/companieshouse/documentsigningrequestconsumer/ApiClientService.java
@@ -1,5 +1,6 @@
 package uk.gov.companieshouse.documentsigningrequestconsumer;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.api.InternalApiClient;
 import uk.gov.companieshouse.sdk.manager.ApiSdkManager;
@@ -7,10 +8,11 @@ import uk.gov.companieshouse.sdk.manager.ApiSdkManager;
 @Component
 public class ApiClientService {
 
+    @Value("${internal.api.url}") String internalApiUrl;
+
     public InternalApiClient getInternalApiClient() {
         final var client = ApiSdkManager.getPrivateSDK();
-        // TODO DCAC-151 Replace this Tilt-specific override with an environment variable.
-        client.setInternalBasePath(client.getBasePath());
+        client.setInternalBasePath(internalApiUrl);
         return client;
     }
 }
